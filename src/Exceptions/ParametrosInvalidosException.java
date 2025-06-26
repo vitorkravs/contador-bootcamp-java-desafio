330e7ac6063e446bf2f8abc1c2c47ddab15632fa
Vitor Kravszenko <vitor.kravs@accist.com.br>
2025-06-26 00:51:24 -0300

    feat: criar o package de exception e adicionar classe ParametrosInvalidosException para trativa de erro

---
+++ src/Exceptions/ParametrosInvalidosException.java
@@ -0,0 +1,7 @@
+package Exceptions;
+
+public class ParametrosInvalidosException extends RuntimeException {
+    public ParametrosInvalidosException(String message) {
+        super(message);
+    }
+}
